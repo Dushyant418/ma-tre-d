9009a6a0fe59386f55b62e691dd1f053187b744c
Dushyant418 <dushyantlochab335@gmail.com>
2024-04-01 15:39:49 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+const menuicon = document.querySelector(".menuicon");
+const menulist = document.querySelector(".menulist");
+const navlink = document.querySelectorAll(".nav-links")
+navlink.forEach(a => a.addEventListener("click", () => {
+    menulist.classList.toggle("show");
+    document.body.classList.toggle("overflow-hidden");
+    menuicon.classList.toggle("show");
+}))
+menuicon.addEventListener("click", () => {
+    menulist.classList.toggle("show");
+    document.body.classList.toggle("overflow-hidden");
+    menuicon.classList.toggle("show");
+});
+
+
+
+$('.sldr').slick({
+    dots: false,
+    infinite: true,
+    arrows: true,
+    speed: 300,
+    prevArrow: ".pre-btn",
+    nextArrow: ".next-btn",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                autoplay: true,
+
+            }
+        },
+    ]
+});
